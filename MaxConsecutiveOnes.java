eed79efac12fec6addd892f773d32e18c8d340f4
amit sahu <amitsahu6673@gmail.com>
2020-11-09 17:38:57 +0530

    Leet Code Max Consecutive Ones Solution

---
+++ MaxConsecutiveOnes.java
@@ -0,0 +1,22 @@
+public class MaxConsecutiveOnes {
+    public static void main(String[] args){
+        int nums[] = {0,1,1,0,1,1,1};
+        System.out.println("Most consecutive Number Of Ones Are :"+findMaxConsecutiveOnes(nums));
+    }
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int countofOnes = 0;
+        int maxCountOfOnes = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                countofOnes++;
+                if(maxCountOfOnes<countofOnes){
+                    maxCountOfOnes = countofOnes;
+                }
+            }
+            else{
+                countofOnes=0;
+            }
+        }
+        return maxCountOfOnes;
+    }
+}
